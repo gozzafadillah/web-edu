18a58bd91f54a24aac93ff602215271f65792e7b
gozzafadillah <gozzafadillah@gmail.com>
2023-07-14 23:25:37 +0700

    delete library useless

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -2,7 +2,6 @@ import Navbar from "../components/Navbar";
 import Jumbotron from "../components/Jumbotron";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import { Analytics } from "@vercel/analytics/react";
 
 const HomePages = () => {
   return (
